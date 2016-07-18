df559eaf452b2dd2876704aae147885b116ecbbc
артур <mega9594@Rambler.ru>
2016-07-19 01:59:35 +0300

    Initial commit

---
+++ src/com/goit/SquareSum.java
@@ -0,0 +1,7 @@
+package com.goit;
+
+import java.util.concurrent.ExecutionException;
+
+public interface SquareSum  {
+    long getSquareSum(int[] values, int numberOfThreads) throws ExecutionException, InterruptedException;
+}
